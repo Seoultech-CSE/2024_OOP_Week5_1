9b9874e6b8dbc027cac3d8e17daea060866e6dc9
LeeHyunWon999 <142217859+LeeHyunWon999@users.noreply.github.com>
2024-03-25 14:01:04 +0900

    Update CountNumbers.java

--- CountNumbers.java
+++ CountNumbers.java
@@ -1,7 +1,7 @@
 class CountNumbers {
   public static void main (String[] args) {
     Scanner input = new Scanner(System.in);
-    int[] counts = new int[50];
+    int[] counts = new int[51];
 
     System.out.print("Enter the integers : ");
 
